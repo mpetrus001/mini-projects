649b080b1f49129f55edc41dfd10bbea95fc5eb8
Matthew Petrus <matthewdavidpetrus@gmail.com>
2020-03-14 11:28:24 -0500

    add nodemon with ts-node

--- sign-in/src/server/server.ts
+++ sign-in/src/server/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-	res.send("The sedulous hyena ate the antelope!");
+	res.send("Hello, world!");
 });
 app.listen(port, err => {
 	if (err) {
